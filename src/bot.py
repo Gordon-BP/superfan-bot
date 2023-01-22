b9789211b3be28423b0817f6420f28fb6630d7af
Gordon Clark <gordon.clark@botpress.com>
2023-01-21 23:43:36 -0500

    got started on the new create data fn

--- src/bot.py
+++ src/bot.py
@@ -1,13 +1,15 @@
-from discord import Intents, utils
+import discord
 from discord.ext import commands
 import dotenv
+import cohere
+from createData import create_index
 import os
 import logging
 
 # Init the bot with the right discord permissions
 dotenv.load_dotenv()
 token = os.environ["DISCORD_TOKEN"]
-intents = Intents.default()
+intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
@@ -15,7 +17,8 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 # Friendly startup message
 @bot.event
 async def on_ready():
-    utils.setup_logging(level=logging.INFO, root=False)
+    # Let's set up logging!
+    discord.utils.setup_logging(level=logging.INFO, root=False)
     global botLogger
     botLogger = logging.getLogger(f"{bot.user}")
     c_handler = logging.StreamHandler()
@@ -24,14 +27,41 @@ async def on_ready():
     botLogger.addHandler(c_handler)
     botLogger.setLevel(logging.INFO) # <-- THIS!
 
+    # Next let's initialize cohere!
+    if(os.environ['COHERE_API_KEY']):
+        global co 
+        co = cohere.Client(os.environ['COHERE_API_KEY'])
+
     botLogger.info(f'The bot is ready!')
 
 # Command just to test if the bot is onling
 @bot.command()
-async def repeat(ctx, *args):
+async def repeat(ctx: discord.ext.commands.context.Context , *args):
     botLogger.info('User requested repeat: {}'.format(' '.join(args)))
     await ctx.send('{}'.format(' '.join(args)))
 
-
+@bot.command()
+async def create_dataset(ctx: discord.ext.commands.context.Context, url:str, index:str, **kwargs):
+    """
+    Takes a datasource URL and creates a Pinecone Index vector database out of it!
+    """
+    ctx.send(f'Creating an index for {url}...')
+    # First, let's type check all of our arguments!
+    if not(isinstance(url, str)):
+        await ctx.send(f"The URL is {type(url)} not str")
+        botLogger.error(f"Invalid Parameter: The URL is {type(url)} not str")
+        return
+    elif not(isinstance(index, str)):
+        await ctx.send(f"The Index label is type {type(index)} and not a valid string")
+        botLogger.error(f"Invalid Parameter: The Index label is {type(url)} not str")
+        return
+    else:
+        botLogger.info("Creating index, please wait")
+        #TODO: this is where we unpack and verify the kwargs
+        index = await create_index(url, index)
+        await ctx.send('Index created! Here are the stats:')
+        stats = index.describe_index_stats()
+        await ctx.send(str(stats))
+    
 
 bot.run(token)
--- src/createData.py
+++ src/createData.py
@@ -2,35 +2,40 @@ import pandas as pd
 import re
 import os
 import numpy as np
-from io import StringIO
-import sqlalchemy
+import cohere
 from dotenv import load_dotenv
 import mwparserfromhell
 from pyunpack import Archive
 from bs4 import BeautifulSoup, ResultSet
 import requests
-import logging
 from pathlib import Path
 import pinecone
-import logging as log
+import logging
 from transformers import GPT2TokenizerFast
-from .app import get_embedding
-log = logging.getLogger("uvicorn.info")
-#handler = logging.StreamHandler()
+logging.basicConfig(level=logging.DEBUG)
 os.environ['TOKENIZERS_PARALLELISM'] = 'true'
+load_dotenv()
 #log.addHandler(handler)
 
-def getWikiAsSoup(url:str, filepath:Path)-> BeautifulSoup:
-  log.info(f"Fetching file from {url}...")
-  response = requests.get(url)
-  if(response.status_code != 200):
-    log.error("Provided URL is not valid")
-    raise ValueError 
-  else: 
-    filepath.write_bytes(response.content)
-    Archive(filepath).extractall("./data/")
-    soup = BeautifulSoup(open("./data/wiki.xml"), "lxml")
-    return soup
+def getDataAsSoup(url:str, filepath:Path, filename:str='wiki.xml')-> BeautifulSoup:
+    """
+    Downloads XML data from the supplied URL and turns it into a beautiful soup
+
+    Parameters:
+        url(str): The data source URL
+        filepath(Path): Where the data will be initially downloaded to
+        filename(str): The name of the file used to temporarially store the data
+    """
+    logging.info(f"Fetching file from {url}...")
+    response = requests.get(url)
+    if(response.status_code != 200):
+        logging.error("Provided URL is not valid")
+        raise ValueError 
+    else: 
+        filepath.write_bytes(response.content)
+        Archive(filepath).extractall("./data/")
+        soup = BeautifulSoup(open(f"./data/{filename}"), "lxml")
+        return soup
 
 def cleanData(pages: ResultSet, limit:int = -1) -> pd.DataFrame:
   """
@@ -77,7 +82,7 @@ def cleanData(pages: ResultSet, limit:int = -1) -> pd.DataFrame:
                 collapse=True,
                 keep_template_params=False)
             if((len(text) < 20) or ("REDIRECT" in text)):
-              log.debug(f"Skipping {title} - {heading}")
+              logging.debug(f"Skipping {title} - {heading}")
             dataArr.append({
                   "id":int(id),
                   "title":str(title),
@@ -93,7 +98,7 @@ def cleanData(pages: ResultSet, limit:int = -1) -> pd.DataFrame:
                 keep_template_params=False)
             # There's a few criteria for whether or not we want to keep this data
             if((heading in badHeadings) or (len(text) < 20) or ("REDIRECT" in text)):
-              log.debug(f"Skipping {title} - {heading}")
+              logging.debug(f"Skipping {title} - {heading}")
             else:
               dataArr.append({
                   "id":int(id),
@@ -119,7 +124,7 @@ def cleanData(pages: ResultSet, limit:int = -1) -> pd.DataFrame:
                   heading = re.sub(r"=",'', subheader)
                   text = mwparserfromhell.parse(subsubcontent[0]).strip_code()
                   if((heading in badHeadings) or (len(text) < 20) or ("REDIRECT" in text)):
-                    log.debug(f"Skipping {title} - {subheader} - {heading}")
+                    logging.debug(f"Skipping {title} - {subheader} - {heading}")
                   dataArr.append({
                     "id":int(id),
                     "title":str(title),
@@ -136,7 +141,7 @@ def cleanData(pages: ResultSet, limit:int = -1) -> pd.DataFrame:
                         "text": str(re.sub("\n",'',mwparserfromhell.parse(subsubsubcontent).strip_code()))
                     })
       except:
-        log.warn(f"Something went wrong processing {page.title.contents[0]}")
+        logging.warn(f"Something went wrong processing {page.title.contents[0]}")
         counter +=1
   return pd.DataFrame.from_records(dataArr)
 
@@ -156,15 +161,16 @@ def fast_chonk(row:pd.Series, tokenizer:GPT2TokenizerFast) -> list[dict]:
             tokens: 79
         }]
     """
-    log.debug(f"Chunking {row.title} - {row.heading}...")
+    logging.debug(f"Chunking {row.title} - {row.heading}...")
     sentences = row.text.split(". ")
     bigSentence = ""
     chunks = []
     for idx, sentence in enumerate(sentences):
         length = len(tokenizer.encode(sentence))
-        if len(bigSentence) + length < 400:
+        if len(tokenizer.encode(bigSentence)) + length <= 512:
             bigSentence = bigSentence + ". " + sentence
         else:
+            bigSentence = sentence
             chunks.append({
                 "id":int(row.id),
                 "title":str(row.title),
@@ -172,7 +178,6 @@ def fast_chonk(row:pd.Series, tokenizer:GPT2TokenizerFast) -> list[dict]:
                 "text": str(bigSentence),
                 "tokens": int(len(tokenizer.encode(bigSentence)))
             })
-            bigSentence = sentence
 
     return chunks
 
@@ -188,7 +193,7 @@ def compute_cost_estimate(df: pd.DataFrame) -> float:
     """
     return (df.tokens.sum()/1000)*0.0004
 
-def build_embeddings_table(df:pd.DataFrame, dbPrefix:str, pool:sqlalchemy.engine.Engine)-> pd.DataFrame:
+def build_embeddings_table(df:pd.DataFrame, dbPrefix:str)-> pd.DataFrame:
     """
     Uses Cohere's embeddings endpoint to embed content and loads it into a new database table.
 
@@ -202,8 +207,8 @@ def build_embeddings_table(df:pd.DataFrame, dbPrefix:str, pool:sqlalchemy.engine
     """
 
 
-def create_dataset(dataSource:str, indexName:str, dimension:int = 1024, metric:str='cosine', 
-    overrideIndex:bool=False) -> pinecone.Index:
+def create_index(dataSource:str, indexLabel:str, dimension:int = 1024, metric:str='cosine', 
+    overrideIndex:bool = False, maxTokens:int = 512, minTokens:int = 10) -> pinecone.Index:
     """
     This is supposed to be the main function that:
     1. Fetches the XML data from the provided URL ✅
@@ -214,44 +219,78 @@ def create_dataset(dataSource:str, indexName:str, dimension:int = 1024, metric:s
     
     Parameters:
         dataSource(str): The url where the wiki's XML is hosted. Usually an amazon s3 container
-        indexName(str): The name of the pinecone index where data is stores 
+        indexLabel(str): The name of the pinecone index where data is stores 
         dimension(int): How long your vectors are. Default value is 1024 to match cohere's small model
         metric(str): How to calculate vector similarity. Options include cosine, dotproduct, or euclidean
         overrideIndex(bool): If the index already exists, setting this to True will have the system ovrride it. False by default
-    
+        maxTokens(int): The maximum token length for a chunk of text data. Pincone advises 512 so that's the default value
+        minTokens(int): The minimum token length for a chunk of text data. 10 by default for no good reason.
+
     Returns
         pinecone.Index: an Index that can access the pinecone database
     """
-    pool = connect_unix_socket()
-    meta = sqlalchemy.MetaData(bind=pool)
-    articles_table = sqlalchemy.Table(f"{dbPrefix}_articles", meta)
-    embeddings_table = sqlalchemy.Table(f"{dbPrefix}_embeddings", meta)
-    log.info(f"Status for articles table {articles_table.name} is {articles_table.exists()}")
-    log.info(f"Status for embeddings table {embeddings_table.name} is {embeddings_table.exists()}")
-    log.info(f"Override tables is {overrideTables}")
-    if(not(articles_table.exists()) or overrideTables):
-        log.info(f"Creating new articles table or overriding existing one...")
-        articles_df = build_articles_table(url, pool, dbPrefix)
-       # log.info("Using existing articles table...")
-       # dumbPool = connect_unix_socket(future=False)
-       # articles_df = pd.read_sql_query(f"SELECT * FROM {dbPrefix}_articles", dumbPool)
-        log.info(f"Creating new embeddings table or overriding existing one...")
-        embeddings_df = build_embeddings_table(articles_df, dbPrefix, pool)
-        # Build a nice little JSON object to return
-    else:
-        log.info("Tables already exist, loading them into memory....")
-        with pool.connect() as conn:
-            articles_df = pd.read_sql_table(f"{dbPrefix}_articles", conn)
-            embeddings_df = pd.read_sql_table(f"{dbPrefix}_embeddings", conn)
-            embeddings_df['vec'] = embeddings_df.vec.apply(lambda x: [float(i) for i in x[1:-1].split(",")])
+    if(not(overrideIndex)):
+        logging.info(f"Creating new Index or overriding existing one...")
+        # First we need to load and clean the data
+        soup = getDataAsSoup(dataSource, Path('./data/wiki.xml.7z')) #Fetch the data
+        df = cleanData(soup.find_all('page'))
+        df = df.drop(df.loc[df['text'].str.contains(r"REDIRECT", re.IGNORECASE)].index).reset_index()
+        tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+        df['tokens'] = df.text.apply(lambda x:int(len(tokenizer.encode(x)))) #Get token count to find articles with long subsections
+        df_short = df.loc[(df.tokens <= maxTokens) & (df.tokens >= minTokens)]
+        chunks = []
+        for _, row in df.loc[df.tokens >= maxTokens].iterrows():
+            chunks = chunks + fast_chonk(row, tokenizer)
+        df_chunks = pd.DataFrame.from_records(chunks)
+        df = pd.concat([df_short, df_chunks])
+        df.drop("index", axis=1, inplace=True)
+        df.reset_index(drop=True)  
+        # limit to 100 rows for testing
+        df = df.iloc[:100]
+        logging.info(f"Articles data created with shape {df.shape}")
+        #TODO: Delete the .xml and .7z files after loading them
+        logging.info('aaaaAAAAAAaaaAAAAAA\n\n\n\naaaaaAAAAAaaaaAAaAAAa')
+        # Now we embed it!
+        # setting it to only embed the first 10 for testing purposes
+        logging.info('Getting the Embeddings now....')
+        load_dotenv()
+        co = cohere.Client(os.environ['COHERE_API_KEY'])
+        logging.info("We have the key, now calling cohere...")
+        embed = co.embed(
+            texts=df['text'],
+            model='small',
+            truncate='LEFT'
+        ).embeddings
+        shape = np.array(embed).shape
+        logging.debug("Cohere part is done")
+        # Start getting Pinecone set up
+        logging.info("Making the Pinecone now....")
+        load_dotenv()
+        pinecone.init(os.environ['PINECONE_API_KEY'], environment='us-west1-gcp')
+        # if the index does not exist, we create it
+        if indexLabel not in pinecone.list_indexes():
+            pinecone.create_index(
+                indexLabel,
+                dimension=shape[1],
+                metric='dotproduct'
+            )
+        # connect to index
+        index = pinecone.Index(indexLabel)
+        batch_size = 128
+        # Now we start to laod the embeddings....
+        ids = df.loc['id'].to_list()
+        # create list of metadata dictionaries
+        meta = [[{'title': title} for title in df.loc['title']],
+        [{'heading': heading} for heading in df.loc['heading']],
+        [{'text': text} for text in df.loc['text']]]
+        # create list of (id, vector, metadata) tuples to be upserted
+        to_upsert = list(zip(ids, embed, meta))
 
-    return {
-            f"{dbPrefix}_articles":{
-                "tableStatus":articles_table.exists(),
-                "dataFrame": articles_df
-        },
-            f"{dbPrefix}_embeddings":{
-                "tableStatus":embeddings_table.exists(),
-                "dataFrame":embeddings_df
-            }
-        }
+        for i in range(0, shape[0], batch_size):
+            i_end = min(i+batch_size, shape[0])
+            index.upsert(vectors=to_upsert[i:i_end])
+
+        # let's view the index statistics
+        print(index.describe_index_stats())
+
+    return index
